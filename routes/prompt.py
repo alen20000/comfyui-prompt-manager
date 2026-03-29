afc5c0a7d5f0170f7689ad20362b223d3b732c82
cliff <alen20000>
2026-03-29 20:18:36 +0800

    WIP:完成HTMX與Flask的串接；完成前端觸發與後端資料庫BD查詢的測試

--- routes/prompt.py
+++ routes/prompt.py
@@ -1,8 +1,9 @@
-from flask import Blueprint,render_template
-# 注意：這裡我們先不 import Database，先讓網址跑通！
+from flask import Blueprint,render_template,jsonify
+from database import Database
 
-# 1. 定義藍圖零件 (變數名, 內部身分證)
 prompt_bp = Blueprint('prompt', __name__)
+
+db = Database()    
 
 
 @prompt_bp.route('/', methods=['GET'])
@@ -11,3 +12,8 @@ def test_connection():
     Main page    
     '''
     return render_template('index.html')
+
+@prompt_bp.route('/get_all_prompts', methods=['GET'])
+def get_all_prompts():
+    data = db.get_all_prompts()
+    return render_template('partials/prompt_items.html', prompts=data)
